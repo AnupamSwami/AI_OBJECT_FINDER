9db10b24b3a44794683f09053f572c10703b45ec
Anupam <97234085+AnupamSwami@users.noreply.github.com>
2024-01-16 19:18:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 status = "" ;
 object = "" ;
+objects = [] ;
 
 function preload() {
 
@@ -14,6 +15,34 @@ function setup() {
 
 function draw() {
     image(video , 0 , 0 , 450 , 350) ;
+    if(status != "") {
+        objectDetector.detect(video , gotResult) ;
+        for(i = 0 ; i < objects.length ; i++ ) {
+            fill('#0000FF') ;
+            stroke('#0000FF') ;
+            noFill() ;
+            percent = floor(objects[i].confidence * 100) ;
+            text(objects[i].label + " " + percent + "%" , objects[i].x + 15 , objects[i].y + 15) ;
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height) ;
+            if(objects[i].label = object) {
+                video.stop() ;
+                objectDetector.detect(gotResult) ;
+                document.getElementById("status").innerHTML = object + " Found" ;
+                var synth = window.speechSynthesis ;
+                speak_data = object + " Found " ;
+                var utterThis = new SpeechSynthesisUtterance(speak_data) ;
+                synth.speak(utterThis) ;
+            }
+        }
+    }
+}
+
+function gotResult(error , results) {
+    if(error) {
+        console.error(error) ;
+    }
+    console.log(results) ;
+    objects = results ;
 }
 
 function start() {
